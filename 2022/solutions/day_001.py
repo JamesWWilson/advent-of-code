5dea6e47116c2826f2295f1b12e3a2153d9e00d4
James Wilson <canadianjw@gmail.com>
2022-12-01 19:18:32 -0800

    Update day_001.py

--- 2022/solutions/day_001.py
+++ 2022/solutions/day_001.py
@@ -1,10 +1,7 @@
-# Puzzle 1
-# 12-01-2022
-
 from utils import * 
 
 # Sum lines in a text file and append to list when blank line appears 
-def calc_calories(data):
+def calc_calories(data): 
     all_cal = []
     elf_cal = 0
     for line in data:
@@ -16,13 +13,13 @@ def calc_calories(data):
     return(all_cal)
 
 #function to return the sum of the largest [x] items in a list 
-def sum_top_x(list, x):
+def sum_top_x(list, x): 
     top_x = sorted(list, reverse = True)[:x]
     if x > 1:
         top_x = sum(top_x)
     return(top_x)
 
 # Load input and solve
-input = Input('001').readlines()
-print(sum_top_x(calc_calories(input),1)) #solution 1a
+input = Input('001').readlines() 
+print(sum_top_x(calc_calories(input),1)) #solution 1a 
 print(sum_top_x(calc_calories(input),3)) #solution 1b
